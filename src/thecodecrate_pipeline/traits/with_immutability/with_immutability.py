7716c9cc3764ea7d04014d09468a6fab80325452
Daniel Loureiro <loureirorg@gmail.com>
2024-09-27 00:57:43 +0000

    🚀 refactor: simplify trait implementation using Protocol and Self
    - Replaced generic class dependencies with Protocol for a cleaner and simpler design
    - Refactored self-return in traits to use `Self` instead of `TParentClass` or explicit class names

--- src/thecodecrate_pipeline/traits/with_immutability/with_immutability.py
+++ src/thecodecrate_pipeline/traits/with_immutability/with_immutability.py
@@ -1,21 +1,14 @@
-from abc import ABC
-from typing import Generic
+from typing import Generic, Protocol
 from thecodecrate_builderable import (
     WithImmutability as BuilderableWithImmutability,
 )
-
-from .parent_class import TParentClass
 from ...core.pipeline.payload import TPayload
 from ...core.pipeline.pipeline_callable import PipelineCallable
 
 
 class WithImmutability(
-    BuilderableWithImmutability[
-        TParentClass,  # type: ignore
-        TPayload,
-        PipelineCallable[TPayload],
-    ],
-    Generic[TParentClass, TPayload],
-    ABC,
+    BuilderableWithImmutability[TPayload, PipelineCallable[TPayload]],
+    Generic[TPayload],
+    Protocol,
 ):
     pass
