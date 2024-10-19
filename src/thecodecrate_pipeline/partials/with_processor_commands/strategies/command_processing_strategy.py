b423b3e0f9daa850ab8cac41a6338ad88aca6321
Daniel Loureiro <loureirorg@gmail.com>
2024-10-19 08:01:38 +0000

    :recycle: refactor: extract command instance creation into a separate method
    - Refactored `CommandProcessingStrategy` to extract the command instance creation logic into a dedicated method `_make_command`.
    - Improved code readability and maintainability by separating concerns within the processing strategy.
    - The `_make_command` method now handles instantiation of the command, allowing better customization and reuse.

--- src/thecodecrate_pipeline/partials/with_processor_commands/strategies/command_processing_strategy.py
+++ src/thecodecrate_pipeline/partials/with_processor_commands/strategies/command_processing_strategy.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from ..command_interface import CommandInterface
 from ...with_base.stage_callable import StageCallable
 from ...with_base.types import T_in, T_out
 from ..processing_strategy import ProcessingStrategy
@@ -13,15 +14,24 @@ class CommandProcessingStrategy(ProcessingStrategy[T_in, T_out]):
         *args: Any,
         **kwds: Any,
     ) -> T_out:
+        command = self._make_command(payload, stages, *args, **kwds)
+
+        return await command.execute(*args, **kwds)
+
+    def _make_command(
+        self,
+        payload: T_in,
+        stages: list[StageCallable],
+        *args: Any,
+        **kwds: Any,
+    ) -> CommandInterface[T_in, T_out]:
         if not self.processor.command_class:
             raise ValueError("Command class is not set")
 
-        command = self.processor.command_class(
+        return self.processor.command_class(
             processor=self.processor,
             payload=payload,
             stages=stages,
             *args,
             **kwds,
         )
-
-        return await command.execute(*args, **kwds)
