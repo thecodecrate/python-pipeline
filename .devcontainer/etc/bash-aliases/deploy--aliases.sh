c6db0efb1b7a1cf882daf95d23d2ccaa60b48aae
Daniel Loureiro <loureirorg@gmail.com>
2024-12-15 00:38:13 +0000

    🔧 chore: clean up cache-related aliases and .gitignore
    - Rename bash aliases for better clarity
    - Update cache-cleaning aliases
    - Adjust `.gitignore` to properly ignore cache files

--- .devcontainer/etc/bash-aliases/deploy--aliases.sh
+++ .devcontainer/etc/bash-aliases/deploy--aliases.sh
@@ -1,5 +1,5 @@
 # Alias for publishing the package to pypi
-alias clean_dist='find . -name "dist"  | xargs rm -rf;'
+alias _clean_dist='find . -type d -name "dist" -exec rm -rf {} +'
 alias bump='bumpver update --minor'
-alias build='clean_dist; uv build'
+alias build='_clean_dist; uv build'
 alias deploy='uv publish'
