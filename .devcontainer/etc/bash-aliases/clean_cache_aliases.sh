4a63b02ee1d719e2c2cdca4726e50c6f0c85eb1e
Daniel Loureiro <loureirorg@gmail.com>
2024-12-15 00:38:13 +0000

    🔧 chore: clean up cache-related aliases and .gitignore
    - Rename bash aliases for better clarity
    - Update cache-cleaning aliases
    - Adjust `.gitignore` to properly ignore cache files

--- .devcontainer/etc/bash-aliases/clean_cache_aliases.sh
+++
@@ -1,4 +0,0 @@
-alias rm_pycache='find . -name "__pycache__" | xargs rm -rf;'
-alias rm_egginfo='find . -name "*.egg-info"  | xargs rm -rf;'
-alias rm_dist='find . -name "dist"  | xargs rm -rf;'
-alias clean='rm_pycache; rm_egginfo; rm_dist'
